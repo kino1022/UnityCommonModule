64ef532bf78ba8c45610741cf365c3d140ff4c31
kino1022 <yuta_fujita@outlook.com>
2025-05-25 09:11:32 +0900

    Refresh indent

--- PlayableAnimation/ClipPlayer/AnimationClipPlayer.cs
+++ PlayableAnimation/ClipPlayer/AnimationClipPlayer.cs
@@ -1,15 +1,15 @@
 using System;
 using System.Collections.Generic;
+using Sirenix.OdinInspector;
 using UnityCommonModule.PlayableAnimation.Interface;
 using UnityEngine;
 using UnityEngine.Animations;
 using UnityEngine.Playables;
 
-namespace UnityCommonModule.PlayableAnimation.ClipPlayer
-{
+namespace UnityCommonModule.PlayableAnimation.ClipPlayer {
     [Serializable]
-    public class AniamtionClipPlayer
-    {
+    public class AnimationClipPlayer {
+        
         protected IGraphHolder m_graph;
 
         protected IOutputHolder m_output;
@@ -17,22 +17,28 @@ namespace UnityCommonModule.PlayableAnimation.ClipPlayer
         protected AnimationClip m_clip;
 
         protected AnimationClipPlayable m_playable;
-
-        protected List<PlayableBehaviour> m_behaviours;
-
+        
+        //-----------------------open methods-------------------------------
+        
+        [Button("Start Animation")]
+        public void StartAnimation() {
+            m_playable.Play();
+        }
+        [Button("Stop Animation")]
+        public void StopAnimation() {
+            m_playable.Pause();
+        }
 
         //-----------------------SetUp methods------------------------------
 
-        protected AnimationClipPlayable CreateClipPlayable()
-        {
+        protected AnimationClipPlayable CreateClipPlayable() {
             return AnimationClipPlayable.Create(m_graph.GetPlayableGraph(), m_clip);
         }
 
         //-----------------------Connect methods-----------------------------
 
-        protected void ConnectOutput()
-        {
-            
+        protected void ConnectOutput() {
+            m_output.GetPlayableOutput().SetSourcePlayable(m_playable);
         }
     }
 }
--- PlayableAnimation/ClipPlayer/Behaviour/EventReceiverBehaviour.cs
+++ PlayableAnimation/ClipPlayer/Behaviour/EventReceiverBehaviour.cs
@@ -2,15 +2,8 @@ using UnityCommonModule.PlayableAnimation.Interface;
 using UnityEngine.Animations;
 using UnityEngine.Playables;
 
-namespace UnityCommonModule.PlayableAnimation.ClipPlayer.Behaviour
-{
-    public class EventReceiverBehaviour : PlayableBehaviour
-    {
-        public override void ProcessFrame(Playable playable, FrameData info, object playerData)
-        {
-            base.ProcessFrame(playable, info, playerData);
-
-        }
+namespace UnityCommonModule.PlayableAnimation.ClipPlayer.Behaviour {
+    public class EventReceiverBehaviour : PlayableBehaviour {
 
         //-----------------open methods--------------------------------
 
